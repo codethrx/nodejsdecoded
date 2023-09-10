c6f6bbe839150076c0bb4ae83527e35a5b9245f1
Tatheer Mehdi <93590787+codethrx@users.noreply.github.com>
2023-09-10 15:27:57 +0500

    http server created

--- built-in/http-module.js
+++ built-in/http-module.js
@@ -2,8 +2,10 @@ console.log(`Creating a basic http server.`);
 const http = require("http");
 
 const server = http.createServer(function (req, res) {
-  const superhero = { fname: "Bruce", lname: " Wayne" };
-  res.writeHead(201, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(superhero));
+  if (req.url === "/superhero") {
+    const superhero = { fname: "Bruce", lname: " Wayne" };
+    res.writeHead(201, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(superhero));
+  }
 });
 server.listen(4000, () => console.log(`Server runnign`));
